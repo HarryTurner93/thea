fd36c645e07f177e85a8e4fc36a1a0882f4c6bb8
Henry Turner <harry.turner@rovco.com>
2021-08-04 16:04:00 +0100

    Changed config approach.

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,3 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL as string;
-export const S3_URL = process.env.REACT_APP_S3_URL as string;
-export const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string;
+export const API_URL = 'http://localhost:8000/web';
+export const S3_URL = 'http://localstack:4566';
+export const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoiaHR1cm5lcjMwIiwiYSI6ImNrNjI5ZHloMTBhcjYzb3BlMnVpOG80bmwifQ.eKnvR1lWydBWdG6VX7VEKA';
